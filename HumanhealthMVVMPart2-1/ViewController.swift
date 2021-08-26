81c506f8308fa23c630c3be6bea904aa2c441c87
Yuta iwasaki <yanyong0127@gmail.com>
2021-08-26 18:33:59 +0900

    Initial Commit

---
+++ HumanhealthMVVMPart2-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HumanhealthMVVMPart2-1
+//
+//  Created by IwasakIYuta on 2021/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
