2dbe3bedee7d3a588b81a1d96310ff61809735ce
Yuta iwasaki <yanyong0127@gmail.com>
2021-08-26 19:24:09 +0900

    大まかなUI設計完了

---
+++ HumanhealthMVVMPart2-1/Controllers/NewPatientsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewPatientsViewController.swift
+//  HumanhealthMVVMPart2-1
+//
+//  Created by IwasakIYuta on 2021/08/26.
+//
+
+import UIKit
+
+class NewPatientsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
